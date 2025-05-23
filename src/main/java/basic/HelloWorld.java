6e5b81b4f621caec6631bd2a0d268ae96be0de4c
KushiLeo <poojithampatel666@gmail.com>
2025-05-23 15:17:43 +0530

    Update HelloWorld.java

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -3,7 +3,7 @@ package basic;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello! Welcome to my first project");
+		System.out.println("Hello! Welcome to my first project");// adding code from c1 branch
 		
 	}
 
